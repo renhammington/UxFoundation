78ac1bfd888480334039ec12a5f17a0c0ba57695
renhammington <renhammington@gmail.com>
2013-10-18 10:58:15 +1300

    Upgraded to MVC5

--- UxFoundation/Global.asax.cs
+++ UxFoundation/Global.asax.cs
@@ -19,7 +19,8 @@ namespace Ux.Mvc
 		{
 			AreaRegistration.RegisterAllAreas();
 
-			WebApiConfig.Register(GlobalConfiguration.Configuration);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            			
 			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
